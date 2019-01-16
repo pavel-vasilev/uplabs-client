125a9a1be982b762c9815a76eafab74196d63dd7
Pavel Vasilev <pavel.v96@yandex.com>
2019-01-16 23:57:06 +0300

    add http method for creating new post

--- lib/network/uplabs_api.dart
+++ lib/network/uplabs_api.dart
@@ -57,4 +57,43 @@ class UplabsApi {
     await client.put(signedUrl, headers: headers, body: body);
     return fileUrl;
   }
+
+  Future<void> createPost(String title, String description, String previewUrl, String sourceUrl) async {
+    var body = {
+      "utf8": "✓",
+      "authenticity_token": "",
+      "post[name]": title,
+      "post[description]": description,
+      "post[tags]": "",
+      "post[demo_url]": "",
+      "post[tools]": "",
+      "post[fonts]": "",
+      "post[makers_twitter]": "",
+      "post[made_by_submitter]": "0",
+      "post[source_file_url]": sourceUrl,
+      "post[free_post]": "1",
+      "post[free_license]": "personal_and_commercial",
+      "post[paid_post]": "0",
+      "post[screenshots]": "",
+      "post[category_id]": "1",
+      "post[custom_preview_url]": previewUrl,
+      "post[source_name]": "image_url",
+      "post[source_format]": "standard",
+      "post[maker_name]": "",
+      "post[label]": "",
+      "post[pattern]": "",
+      "post[section]": "showcase",
+      "post[source_id]": "",
+      "post[source_version]": "",
+      "post[embeddable]": "false",
+      "post[use_ssl]": "false",
+      "post[full_page]": "false",
+      "post[youtube_video_id]": "",
+      "post[url]": "",
+      "post[extra_info]": "{}",
+      "post[payload]": '{"innerState":"initialized","custom_preview_url":$previewUrl,"free_post":true,"paid_post":false,"validationParams":{},"makers_twitter":"","name":$title","url":null,"maker_name":null,"description":$description,"tags":null,"source_id":null,"source_name":"image_url","category_id":1,"label":null,"price":10,"demo_url":null,"screenshots":[],"price_extended":100,"showcase_later":false,"source_format":"standard","source_version":null,"platform":null,"extra_info":{},"embeddable":false,"youtube_video_id":null,"payload":null,"priority":"tomorrow","industry":null,"style":null,"pattern":null,"fonts":[],"plugins":[],"made_by_submitter":false,"tools":[],"use_ssl":false,"challenge_id":null,"source_file_url":$sourceUrl,"section":"showcase","full_page":false,"free_license":null,"share_networks":[]}',
+      "post[showcase_later]": "0",
+    };
+    await client.post('$baseUrl/posts', body: body);
+  }
 }
